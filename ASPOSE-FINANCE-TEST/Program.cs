665b8d201db3be29006d597a07c7df2af1f5a5f5
yiaosun <Yiao.SUN@outlook.com>
2023-11-20 15:07:02 +0100

    add test files

--- ASPOSE-FINANCE-TEST/Program.cs
+++ ASPOSE-FINANCE-TEST/Program.cs
@@ -11,16 +11,19 @@ namespace ASPOSEFINANCETEST
         {
             // Create xbrl from scach
             XBRLGenerator xBRLGenerator = new XBRLGenerator();
-            //xBRLGenerator.Generate();
+            string xsdFile = Path.GetFullPath(FileManager.GetTestFileDir() + "Schema.xsd");
+            string xbrlOutputFile = Path.GetFullPath(FileManager.GetTestFileDir() + "result.xbrl");
+            XBRLGenerator.Generate(xsdFile, xbrlOutputFile);
 
-
-            string xbrlSourceFile = Path.GetFullPath(FileManager.GetTestFileDir() + "result.xml");
+            
+            string xbrlSourceFile = Path.GetFullPath(FileManager.GetTestFileDir() + "result.xbrl");
 
             // I do the validation before other actions
             Console.WriteLine("Validation Start");
             Validation.Xbrl(xbrlSourceFile);
             Console.WriteLine("Validation End");
 
+            /*
             // I can load the xbrl file (.xml)
             XbrlInstance xbrlInstance = XBRLReader.Load(xbrlSourceFile);
             List<Fact> facts = xbrlInstance.Facts;
@@ -29,13 +32,18 @@ namespace ASPOSEFINANCETEST
             facts.ForEach(fact => Console.WriteLine(fact.Name));
             contexts.ForEach(context => Console.WriteLine(context.Period));
             units.ForEach(unit => Console.WriteLine(unit.MeasureQualifiedNames));
-
+            */
 
             // You can convert xbrl file(.xml) to excel file (.xlsx) or ixbrl file (.ixbrl)
-            string excelTargetFile = Path.GetFullPath(FileManager.GetResultFileDir() + "result.xlsx");
-            string ixbrlTargetFile = Path.GetFullPath(FileManager.GetResultFileDir() + "result.ixbrl");
-            //Convertor.To(xbrlSourceFile, excelTargetFile, SaveFormat.XLSX);
-            //Convertor.To(xbrlSourceFile, ixbrlTargetFile, SaveFormat.IXBRL);
+            string excelTargetFile = Path.GetFullPath(FileManager.GetResultFileDir() + "IdScopeContextPeriodStartAfterEnd.xlsx");
+            string ixbrlTargetFile = Path.GetFullPath(FileManager.GetResultFileDir() + "IdScopeContextPeriodStartAfterEnd.ixbrl");
+            Console.WriteLine("Generate Excel file Start");
+            string validatedRXbrlFile = Path.GetFullPath(FileManager.GetTestFileDir() + "IdScopeContextPeriodStartAfterEnd.xml");
+            Convertor.To(validatedRXbrlFile, excelTargetFile, SaveFormat.XLSX);
+            Console.WriteLine("Generate Excel file End");
+            Console.WriteLine("Generate IXBRL file Start");
+            Convertor.To(validatedRXbrlFile, ixbrlTargetFile, SaveFormat.IXBRL);
+            Console.WriteLine("Generate IXBRL file End");
 
             Console.ReadKey();
         }
--- ASPOSE-FINANCE-TEST/XBRLGenerator.cs
+++ ASPOSE-FINANCE-TEST/XBRLGenerator.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using Aspose.Finance.Xbrl;
 
 namespace ASPOSEFINANCETEST
@@ -12,50 +11,36 @@ namespace ASPOSEFINANCETEST
 
 		}
 
-        public void Generate()
+        public static void Generate(string sourceXsd, string outputXbrlFile)
         {
             XbrlDocument document = new XbrlDocument();
             XbrlInstanceCollection xbrlInstances = document.XbrlInstances;
             XbrlInstance xbrlInstance = xbrlInstances[xbrlInstances.Add()];
             SchemaRefCollection schemaRefs = xbrlInstance.SchemaRefs;
-            schemaRefs.Add( @"HelloWorld.xsd", "example",
-                "http://example.com/xbrl/yoyo");
+            schemaRefs.Add(sourceXsd, "example", "http://example.com/xbrl/taxonomy");
+
             SchemaRef schema = schemaRefs[0];
-
-            ContextPeriod contextPeriod = new ContextPeriod(DateTime.Parse("2020-01-01"),
-                DateTime.Parse("2020-02-10"));
-            ContextEntity contextEntity = new ContextEntity("exampleIdentifierScheme",
-                "exampleIdentifier");
-
+            ContextPeriod contextPeriod = new ContextPeriod(DateTime.Parse("2020-01-01"), DateTime.Parse("2020-02-10"));
+            ContextEntity contextEntity = new ContextEntity("exampleIdentifierScheme", "exampleIdentifier");
             Context context = new Context(contextPeriod, contextEntity);
             xbrlInstance.Contexts.Add(context);
 
             Unit unit = new Unit(UnitType.Measure);
-            unit.MeasureQualifiedNames.Add(new QualifiedName("USD", "iso4217",
-                "http://www.xbrl.org/2003/iso4217"));
+            unit.MeasureQualifiedNames.Add(new QualifiedName("USD", "iso4217", "http://www.xbrl.org/2003/iso4217"));
             xbrlInstance.Units.Add(unit);
 
-
-            List<string> listInfoToFill = new List<String>(){"Land","BuildingsNet",
-    "FurnitureAndFixturesNet",
-    "ComputerEquipmentNet",
-    "OtherPropertyPlantAndEquipmentNet",
-    "PropertyPlantAndEquipmentNet" };
-
-            listInfoToFill.ForEach(info =>
+            Concept fixedAssetsConcept = schema.GetConceptByName("fixedAssets");
+            if (fixedAssetsConcept != null)
             {
-                Concept fixedAssetsConcept = schema.GetConceptByName(info);
-                if (fixedAssetsConcept != null)
-                {
-                    Item item = new Item(fixedAssetsConcept);
-                    item.ContextRef = context;
-                    item.UnitRef = unit;
-                    item.Precision = 4;
-                    item.Value = "9999";
-                    xbrlInstance.Facts.Add(item);
-                }
-            });
-            document.Save(@"output\document5.xbrl");
+                Item item = new Item(fixedAssetsConcept);
+                item.ContextRef = context;
+                item.UnitRef = unit;
+                item.Precision = 4;
+                item.Value = "1444";
+                xbrlInstance.Facts.Add(item);
+            }
+            document.Save(outputXbrlFile);
+            Console.WriteLine("XbrlDocument executed successfully.");
         }
     }
 }
